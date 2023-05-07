8c1dee569e4a3f85f3a74a3f631a8af84bf39e19
Amstroy <comepfc131421@163.com>
2023-05-07 18:08:24 +0800

    encoder对比学习能用版

--- SimCLR-master/runEncoder.py
+++ SimCLR-master/runEncoder.py
@@ -73,7 +73,7 @@ def main():
         train_dataset, batch_size=args.batch_size, shuffle=True,
         num_workers=args.workers, pin_memory=True, drop_last=True)
 
-    model = ResNetSimCLR(base_model=args.arch, out_dim=args.out_dim)
+    model = EncoderSimCLR(base_model=args.arch, out_dim=args.out_dim)
 
     optimizer = torch.optim.Adam(model.parameters(), args.lr, weight_decay=args.weight_decay)
 
